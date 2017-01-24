57f37f7b55d86d8691926f12bd7f6b4b28034fab
Ryan Arakawa <arakawa.ryan@gmail.com>
2017-01-24 15:58:24 -0600

    Checkpoint 9 Assignment: completed

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -50,6 +50,10 @@
                             $document.unbind('mouseup.thumb');
                         });
                     }
+                    
+                    scope.thumbStyle = function(event) {
+                        return {left: percentString()};
+                    }
                 }
             };
     }
